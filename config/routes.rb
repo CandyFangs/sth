63f7a34ee5aa735f62770c35d4787101eaba4bd7
Paulina Osiak <paulina.osiak@gmail.com>
2017-07-15 17:37:29 +0200

    Some routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
+  root 'students#index'
+  resources :teachers
+  resources :reports do
+    get :subjects
+  end
 end
