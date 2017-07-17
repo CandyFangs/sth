b699558a558818e373315598b10fa1f52aa856d7
Paulina Osiak <paulina.osiak@gmail.com>
2017-07-17 13:24:22 +0200

    Gem decorator added, avg_notes method in student decorator filled

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,5 +2,6 @@ class ReportsController < ApplicationController
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
 
   def subjects
+    render :subjects
   end
 end
--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,11 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_items.subject_item_notes
+    if notes.length == 0
+      0.0
+    else
+      (notes.sum / notes.length).to_s.round(2).rjust(2, '0')
+    end
   end
 end
