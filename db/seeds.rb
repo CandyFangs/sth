7d85f09168f2186aba4050562fe4f298746f1bdc
Paulina Osiak <paulina.osiak@gmail.com>
2017-07-20 08:23:27 +0200

    Heroku seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,31 @@
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
+first_name = %w(Fred Cut-Me-Own-Heart Rufus Cheery Carrot Nobby Sybil Reg Havelock Sam Mustrum Ponder Magrat Agnes Gytha Eskarina Esmeralda Susan Otto Moist Adora-Belle Rosemary Cohen Jeremy Lobsang Jonathan Tiffany Lady)
+last_name = %w(Colon Dibbler Drumknott Littlebottom Ironfoundersson Nobbs Ramkin Shoe Vetinari Vimes Ridcully Stibbons Garlick Nitt Ogg Smith Weatherwax Sto-Helit Chriek von-Lipvig Dearheart Palm the-Barbarian Clockson Ludd Teatime Aching Margolotta)
+subjects_titles = %w(Math English Physics Chemistry Biology )
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    first_name: first_name.sample,
+    last_name: last_name.sample,
     academic_title: TEACHER_TITLES.sample
   )
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
-    title: Faker::Lorem.sentence,
+    title: subjects_titles.sample
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    first_name: first_name.sample,
+    last_name: last_name.sample,
+    birthdate: Date.today - rand(365*19..365*30)
   )
 end
 
